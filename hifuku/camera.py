dc718da9e61bbc6e9a0092a6fe35679087c47d71
HiroIshida <spitfire.docomo@gmail.com>
2022-11-05 01:21:08 +0900

    Fixbug ray marching add add test

--- hifuku/camera.py
+++ hifuku/camera.py
@@ -1,17 +1,22 @@
 import copy
 import time
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Protocol
 
 import numpy as np
 import skrobot
 from mohou.types import GrayImage, RGBImage
 from skrobot.coordinates.geo import orient_coords_to_axis
-from skrobot.coordinates.math import matrix2quaternion
-from skrobot.model.primitives import Axis, Box, PointCloudLink, Sphere
-from skrobot.sdf.signed_distance_function import SignedDistanceFunction, UnionSDF
+from skrobot.coordinates.math import matrix2quaternion, rotation_matrix_from_axis
+from skrobot.model.primitives import Axis, Box, Coordinates, PointCloudLink, Sphere
+from skrobot.sdf.signed_distance_function import UnionSDF
 from trimesh import PointCloud
 from voxbloxpy.core import CameraPose, EsdfMap
+
+
+class SDF(Protocol):
+    def __call__(self, pts: np.ndarray) -> np.ndarray:
+        ...
 
 
 @dataclass
@@ -44,30 +49,48 @@ class CameraConfig:
     fovy: float = np.deg2rad(43.0)
 
 
+@dataclass
+class RayMarchingConfig:
+    surface_closeness_threashold: float = 1e-3
+    max_iter: int = 30
+    max_dist: float = 2.0
+    terminate_active_rate_threshold: float = 0.005
+
+
 class Camera(Axis):
     config: CameraConfig
 
     def __init__(
-        self, camera_pos: Optional[np.ndarray] = None, config: Optional[CameraConfig] = None):
+        self, camera_pos: Optional[np.ndarray] = None, config: Optional[CameraConfig] = None
+    ):
         if config is None:
             config = CameraConfig()
         super().__init__(axis_radius=0.02, axis_length=0.3, pos=camera_pos)
         self.config = config
 
-    def look_at(self, p: np.ndarray) -> None:
+    def look_at(self, p: np.ndarray, horizontal: bool = True) -> None:
         if np.all(p == self.worldpos()):
             return
         diff = p - self.worldpos()
         orient_coords_to_axis(self, diff, axis="x")
+        if horizontal:
+            co = Coordinates(
+                pos=self.worldpos(), rot=rotation_matrix_from_axis(diff, [0, 0, 1], axes="xz")
+            )
+            self.newcoords(co)
 
     def generate_point_cloud(
-        self, sdf: SignedDistanceFunction, max_dist: float = 5.0
+        self,
+        sdf: SDF,
+        rm_config: RayMarchingConfig,
+        hit_only: bool = False,
     ) -> np.ndarray:
+        """Generate a point cloud wrt global coordinate"""
         co_proj_plane = self.copy_worldcoords()
-        co_proj_plane.translate(np.array([max_dist, 0.0, 0.0]))
+        co_proj_plane.translate(np.array([1.0, 0.0, 0.0]))
 
-        half_width = np.tan(self.config.fovx * 0.5) * max_dist
-        half_height = np.tan(self.config.fovy * 0.5) * max_dist
+        half_width = np.tan(self.config.fovx * 0.5)
+        half_height = np.tan(self.config.fovy * 0.5)
 
         wlin = np.linspace(-half_width, half_width, self.config.resolx)
         hlin = np.linspace(-half_height, half_height, self.config.resoly)
@@ -85,21 +108,24 @@ class Camera(Axis):
         norms = np.linalg.norm(ray_directions, axis=1)
         ray_directions_unit = ray_directions / norms[:, None]
 
-        dists = self.ray_marching(ray_starts, ray_directions_unit, sdf, 1e-3, max_dist=max_dist)
+        dists = self.ray_marching(ray_starts, ray_directions_unit, sdf, rm_config)
         clouds = ray_starts + dists[:, None] * ray_directions_unit
-        # return clouds[dists < max_dist - 1e-3]
+
+        if hit_only:
+            hit_indices = dists < rm_config.max_dist - 1e-3
+            clouds = clouds[hit_indices]
         return clouds
 
     @staticmethod
     def ray_marching(
-        pts_starts, direction_arr_unit, f_sdf, threshold, max_iter: int = 30, max_dist: float = 5.0
+        pts_starts, direction_arr_unit, f_sdf, rm_config: RayMarchingConfig
     ) -> np.ndarray:
         pts_starts = copy.deepcopy(pts_starts)
         ray_tips = pts_starts
         n_point = len(pts_starts)
         active_flags = np.ones(n_point).astype(bool)
         frying_dists = np.zeros(n_point)
-        for _ in range(max_iter):
+        for _ in range(rm_config.max_iter):
             active_indices = np.where(active_flags)[0]
 
             sd_vals_active = f_sdf(ray_tips[active_indices])
@@ -107,20 +133,20 @@ class Camera(Axis):
             ray_tips[active_indices] += diff
             frying_dists[active_indices] += sd_vals_active
 
-            is_close_enough = sd_vals_active < threshold
+            is_close_enough = sd_vals_active < rm_config.surface_closeness_threashold
             active_flags[active_indices[is_close_enough]] = False
 
-            is_far_enough = frying_dists[active_indices] > max_dist
+            is_far_enough = frying_dists[active_indices] > rm_config.max_dist
             active_flags[active_indices[is_far_enough]] = False
-            frying_dists[active_indices[is_far_enough]] = max_dist
+            frying_dists[active_indices[is_far_enough]] = rm_config.max_dist
             ray_tips[active_indices[is_far_enough]] = np.inf
 
             active_ratio = np.sum(active_flags) / n_point
-            if active_ratio < 0.005:
+            if active_ratio < rm_config.terminate_active_rate_threshold:
                 break
 
-        # frying_dists[active_indices] = np.inf
-        modified_dists = np.minimum(frying_dists, 5.0)
+        frying_dists[active_indices] = rm_config.max_dist  # type: ignore
+        modified_dists = np.minimum(frying_dists, rm_config.max_dist)
         return modified_dists
 
     def get_voxbloxpy_camera_pose(self) -> CameraPose:
@@ -131,7 +157,7 @@ class Camera(Axis):
 
 
 def create_synthetic_esdf(
-    sdf: SignedDistanceFunction,
+    sdf: SDF,
     camera: Camera,
     esdf: Optional[EsdfMap] = None,
     max_dist: float = 2.5,
@@ -143,7 +169,7 @@ def create_synthetic_esdf(
     std_trans = 0.1
     cloud_list = []
     2 + np.random.randint(4)
-    for _ in range(5):
+    for _ in range(1):
         camera_random = copy.deepcopy(camera)
         camera_random.translate(np.random.randn(3) * std_trans)
         camera_random.rotate(np.random.randn() * std_rotate, axis="z")
@@ -160,8 +186,7 @@ def create_synthetic_esdf(
 
 
 if __name__ == "__main__":
-    config = CameraConfig(resolx=320, resoly=240)
-    camera = Camera(np.array([0.0, 0.0, 0.0]), config=config)
+    camera = Camera(np.array([0.0, 0.0, 0.5]))
     camera.look_at(np.array([1.0, 0.0, 0.0]))
     sphere = Sphere(radius=0.7, pos=(2.0, 0.0, 1.0), with_sdf=True)
     box = Box([0.5, 0.5, 0.5], pos=(2.0, 0.0, 0.0), with_sdf=True)
@@ -173,10 +198,10 @@ if __name__ == "__main__":
     )
 
     camera_tmp = copy.deepcopy(camera)
-    esdf = EsdfMap.create(0.02)
-    for _ in range(5):
+    esdf = EsdfMap.create(0.05)
+    for _ in range(10):
         camera_tmp.rotate(np.random.randn() * 0.02, axis="z")
-        pts = camera_tmp.generate_point_cloud(sdf=sdf, max_dist=2.0)
+        pts = camera_tmp.generate_point_cloud(sdf=sdf, max_dist=2.5)
         pose = camera_tmp.get_voxbloxpy_camera_pose()
         ts = time.time()
         esdf.update(pose, pts)
@@ -195,7 +220,7 @@ if __name__ == "__main__":
     viewer.show()
 
     info = esdf.get_voxel_info()
-    grid = info.get_boundary_grid(grid_size=0.04)
+    grid = info.get_boundary_grid(grid_size=0.08)
     grid_sdf = esdf.get_grid_sdf(grid)
     grid_sdf.render_volume(isomin=-0.2, isomax=0.3)
 
