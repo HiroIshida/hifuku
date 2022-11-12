a03dd234ff6c420f751ccb329e67c852e24cf995
HiroIshida <spitfire.docomo@gmail.com>
2022-11-13 08:02:34 +0900

    [experimental] update infer script using pretrained 3dcnn

--- hifuku/script/infer.py
+++ hifuku/script/infer.py
@@ -1,24 +1,36 @@
 import numpy as np
+import torch
 from mohou.file import get_project_path
 from mohou.trainer import TrainCache
 from mohou.utils import detect_device
 from skplan.solver.optimization import IKConfig
 
-from hifuku.nerual import IterationPredictor
+from hifuku.nerual import IterationPredictor, VoxelAutoEncoder
 from hifuku.threedim.tabletop import TabletopIKProblem
+from hifuku.types import ProblemInterface
 
 pp = get_project_path("tabletop_ik")
 
 device = detect_device()
 
-tcache = TrainCache.load(pp, IterationPredictor)
-best_model = tcache.best_model
-best_model.put_on_device(device)
 
 problem = TabletopIKProblem.sample(n_pose=10)
 ik_config = IKConfig(disp=False)
 ret = problem.solve(np.zeros(10), config=ik_config)
-print([e.nit for e in ret])
+print([e.success for e in ret])
 
-out = best_model.infer(problem)
-print(out)
+ae_model: VoxelAutoEncoder = TrainCache.load(pp, VoxelAutoEncoder).best_model
+pred: IterationPredictor = TrainCache.load_latest(pp, IterationPredictor).best_model
+
+
+def infer(problem: ProblemInterface):
+    mesh = problem.get_mesh()
+    mesh = torch.from_numpy(np.expand_dims(mesh, axis=(0, 1))).float()
+    encoded = ae_model.encoder(mesh)
+
+    desc = problem.get_descriptions()
+    desc = torch.from_numpy(np.expand_dims(desc, axis=0)).float()
+    iterval, _ = pred.forward((encoded, desc))
+    print(iterval)
+
+infer(problem)
