7110a44af0403c15df3164c9c732235f0a3f5bbe
HiroIshida <spitfire.docomo@gmail.com>
2022-11-14 08:19:43 +0900

    Show rich information in infer.py

--- hifuku/script/infer.py
+++ hifuku/script/infer.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import torch
 from mohou.file import get_project_path
@@ -14,13 +16,20 @@ pp = get_project_path("tabletop_ik")
 device = detect_device()
 
 
-problem = TabletopIKProblem.sample(n_pose=10)
+problem = TabletopIKProblem.sample(n_pose=1000)
 ik_config = IKConfig(disp=False)
+
+ts = time.time()
 ret = problem.solve_dummy(np.zeros(10), config=ik_config)
+print("time to solve problem {}".format((time.time() - ts) / len(problem.target_pose_list)))
+
 print([e.success for e in ret])
 
 ae_model: VoxelAutoEncoder = TrainCache.load(pp, VoxelAutoEncoder).best_model
 pred: IterationPredictor = TrainCache.load_latest(pp, IterationPredictor).best_model
+
+ae_model.put_on_device(device)
+pred.put_on_device(device)
 
 
 def infer(problem: ProblemInterface):
@@ -32,11 +41,18 @@ def infer(problem: ProblemInterface):
 
     mesh_np = problem.get_mesh()
     mesh = torch.from_numpy(np.expand_dims(mesh_np, axis=(0, 1))).float()
+
+    ts = time.time()
+    desc = desc.to(device)
+    mesh = mesh.to(device)
     encoded: torch.Tensor = ae_model.encoder(mesh)
     encoded_repeated = encoded.repeat(n_batch, 1)
+    print("time to encode mesh {}".format(time.time() - ts))
 
+    ts = time.time()
     iterval, _ = pred.forward((encoded_repeated, desc))
-    print(iterval)
+    print("time to inference {}".format(time.time() - ts))
+    time.sleep(10)
 
 
 infer(problem)
