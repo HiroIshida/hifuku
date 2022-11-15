f8513542c6e8b6ac4d5b64be5dfa0ada4780e441
HiroIshida <spitfire.docomo@gmail.com>
2022-11-16 06:51:14 +0900

    fix: test_neural test

--- tests/test_neural.py
+++ tests/test_neural.py
@@ -14,18 +14,17 @@ def test_network():
 
     n_sol_dim = 10
     model_conf = IterationPredictorConfig(
-        12, ae_config.dim_bottleneck, n_sol_dim, use_solution_pred=True
+        12, ae_config.dim_bottleneck, n_sol_dim, use_solution_pred=False
     )
     model = IterationPredictor(model_conf)
 
     n_batch = 10
-    n_pose_pre_scene = 5
 
     mesh = torch.zeros(n_batch, 1, 56, 56, 28)
     mesh_encoded = ae.encoder(mesh)
-    descriptions = torch.zeros(n_batch, n_pose_pre_scene, 12)
-    solutions = torch.zeros(n_batch, n_pose_pre_scene, n_sol_dim)
-    nits = torch.zeros(n_batch, n_pose_pre_scene)
+    descriptions = torch.zeros(n_batch, 12)
+    solutions = torch.zeros(n_batch, n_sol_dim)
+    nits = torch.zeros(n_batch, 1)
 
     sample = (mesh_encoded, descriptions, nits, solutions)
 
