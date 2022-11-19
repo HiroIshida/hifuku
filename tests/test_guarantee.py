0680cfeea1c2a7ec6b988a6b82754c1f50284b7d
HiroIshida <spitfire.docomo@gmail.com>
2022-11-19 17:33:20 +0900

    test/refactor: move contents of test_utils to test_guarantee

--- tests/test_guarantee.py
+++ tests/test_guarantee.py
@@ -6,17 +6,34 @@ import torch
 from mohou.trainer import TrainConfig
 from skplan.solver.constraint import ConstraintSatisfactionFail
 
+from hifuku.datagen import MultiProcessDatasetGenerator
 from hifuku.library import (
     LibrarySamplerConfig,
     SimpleFixedProblemPool,
     SolutionLibrary,
     SolutionLibrarySampler,
+    compute_real_itervals,
 )
-from hifuku.datagen import MultiProcessDatasetGenerator
 from hifuku.neuralnet import VoxelAutoEncoder, VoxelAutoEncoderConfig
 from hifuku.threedim.tabletop import TabletopPlanningProblem
 from hifuku.types import RawData
 from hifuku.utils import create_default_logger
+
+
+def test_compute_real_itervals():
+    np.random.seed(0)
+    prob_stan = TabletopPlanningProblem.create_standard()
+    res = prob_stan.solve()[0]
+    x_init = res.x
+
+    n_problem = 10
+    problems = [TabletopPlanningProblem.sample(1) for _ in range(n_problem)]
+    maxiter = TabletopPlanningProblem.get_solver_config().maxiter
+
+    itervals_mp = compute_real_itervals(problems, x_init, maxiter, n_process=4)
+    itervals_sp = compute_real_itervals(problems, x_init, maxiter, n_process=1)
+    assert len(itervals_mp) == n_problem
+    assert itervals_mp == itervals_sp
 
 
 def test_MultiProcessDatasetGenerator():
