0d3b2b8ebd1f3484aca71d6d17f8f21f430774ef
HiroIshida <spitfire.docomo@gmail.com>
2022-11-19 23:33:21 +0900

    chore: add docker file and scripts

---
+++ docker/build.sh
@@ -0,0 +1,6 @@
+if [ ! -d .ssh ]
+then
+    echo ".ssh directory not found."
+    ln -sf $HOME/.ssh .
+fi
+tar -chz . |docker build -t hifuku -
---
+++ docker/run_server.sh
@@ -0,0 +1,13 @@
+if [ -z $1 ];
+then
+    port=8080
+else
+    port=$1
+fi
+echo "port is set to $port"
+
+docker run --rm -it -p $port:8080 hifuku:latest \
+    /bin/bash -i -c \
+    'cd ~/scikit-plan && git fetch origin && git checkout origin/master && pip3 install -e . ;\
+    cd ~/hifuku && git fetch origin && git checkout origin/master && pip3 install -e . ;\
+    python3 -m hifuku.http_datagen.server'
