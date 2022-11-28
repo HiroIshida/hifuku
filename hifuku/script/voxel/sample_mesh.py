7b8177affa656e711ba47fe4fbba1252075a6b9e
HiroIshida <spitfire.docomo@gmail.com>
2022-11-29 04:45:29 +0900

    fix: fix mesh sampling in response to api change

--- hifuku/script/voxel/sample_mesh.py
+++ hifuku/script/voxel/sample_mesh.py
@@ -3,8 +3,10 @@ import warnings
 import numpy as np
 from mohou.file import get_project_path
 
-from hifuku.datagen import MultiProcessBatchProblemSolver
+from hifuku.datagen import DistributeBatchProblemSampler, MultiProcessBatchProblemSolver
+from hifuku.pool import SimpleIteratorProblemPool
 from hifuku.threedim.tabletop import TabletopMeshProblem
+from hifuku.utils import create_default_logger
 
 warnings.filterwarnings("ignore", message="Values in x were outside bounds during")
 warnings.filterwarnings("ignore", message="texture specified in URDF is not supported")
@@ -12,9 +14,13 @@ warnings.filterwarnings("ignore", message="texture specified in URDF is not supp
 np.random.seed(0)
 
 if __name__ == "__main__":
-    n_problem = 20000
+    n_problem = 1000
     pp = get_project_path("tabletop_mesh")
+    logger = create_default_logger(pp, "mesh_generation")
     cache_base_path = pp / "cache"
     cache_base_path.mkdir(exist_ok=True, parents=True)
-    gen = MultiProcessBatchProblemSolver[TabletopMeshProblem]()
-    gen.solve_batch(np.zeros(0), n_problem, 0, cache_base_path)
+    sampler = DistributeBatchProblemSampler[TabletopMeshProblem]()
+    pool = SimpleIteratorProblemPool(TabletopMeshProblem, n_problem_inner=0)
+    problems = sampler.sample_batch(n_problem, pool.as_predicated())
+    solver = MultiProcessBatchProblemSolver[TabletopMeshProblem]()  # this doesn't actually solve
+    solver.create_dataset(problems, [np.zeros(0)] * len(problems), cache_base_path, None)
