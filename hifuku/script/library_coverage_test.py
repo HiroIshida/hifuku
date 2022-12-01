11fb67accc0edfac2a2edf133c027063d33632a2
HiroIshida <spitfire.docomo@gmail.com>
2022-12-01 18:51:07 +0900

    fix: fix random seed in coverage test

--- hifuku/script/library_coverage_test.py
+++ hifuku/script/library_coverage_test.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import torch
 
 from hifuku.datagen import MultiProcessBatchProblemSolver
@@ -7,8 +8,9 @@ from hifuku.library import SolutionLibrary
 from hifuku.pool import SimpleFixedProblemPool
 from hifuku.threedim.tabletop import TabletopPlanningProblem
 
+np.random.seed(0)
 p = Path("~/.mohou/tabletop_solution_library").expanduser()
-validation_pool = SimpleFixedProblemPool.initialize(TabletopPlanningProblem, 2000)
+validation_pool = SimpleFixedProblemPool.initialize(TabletopPlanningProblem, 100)
 lib = SolutionLibrary.load(p, TabletopPlanningProblem, device=torch.device("cpu"))[0]
 solver = MultiProcessBatchProblemSolver[TabletopPlanningProblem]()
 coverage = lib.measure_full_coverage(validation_pool, solver)
