af0003704cf714d9c3101214bbdd6a3a76e994dd
HiroIshida <spitfire.docomo@gmail.com>
2022-12-04 06:57:01 +0900

    example: udpate exaple of voxblox problem

--- example/pr2_tabletop_world.py
+++ example/pr2_tabletop_world.py
@@ -2,24 +2,31 @@ import time
 
 import numpy as np
 import skrobot
+from skplan.viewer.skrobot_viewer import set_robot_config
 
+from hifuku.threedim.robot import setup_kinmaps, setup_pr2
 from hifuku.threedim.tabletop import VoxbloxTabletopPlanningProblem
 
-np.random.seed(4)
-use_base = True
+np.random.seed(8)
 
-robot_model = skrobot.models.PR2()
-robot_model.reset_manip_pose()
+pr2 = setup_pr2()
+efkin, colkin = setup_kinmaps()
 
 # create grid sdf using voxbloxpy
 problem = VoxbloxTabletopPlanningProblem.sample(1)
+res = problem.solve()[0]
+assert res.success
 
-visualize = True
-if visualize:
-    viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
-    problem.add_elements_to_viewer(viewer)
-    viewer.add(robot_model)
-    viewer.show()
-    problem.grid_sdf.render_volume
-    problem.grid_sdf.render_volume(isomin=-0.1, isomax=0.1)
-    time.sleep(30)
+viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
+problem.add_elements_to_viewer(viewer)
+viewer.add(pr2)
+viewer.show()
+
+problem.grid_sdf.render_volume
+problem.grid_sdf.render_volume(isomin=-0.1, isomax=0.1)
+
+for q in res.traj_solution:
+    set_robot_config(pr2, efkin.control_joint_names, q, with_base=True)
+    viewer.redraw()
+    time.sleep(0.2)
+time.sleep(30)
