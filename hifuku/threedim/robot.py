cbaa13ff2ce888e82a10edb7835ae903b937d1f6
HiroIshida <spitfire.docomo@gmail.com>
2022-12-06 14:37:33 +0900

    perf: cache pr2 bounds

--- hifuku/threedim/robot.py
+++ hifuku/threedim/robot.py
@@ -6,11 +6,12 @@ from skplan.kinematics import (
     ArticulatedEndEffectorKinematicsMap,
 )
 from skplan.robot.pr2 import PR2Paramter
+from skplan.space import Bounds
 from skrobot.models.pr2 import PR2
 
 from hifuku.threedim.camera import Camera
 
-_cache = {"kinmap": None, "pr2": None}
+_cache = {"kinmap": None, "pr2": None, "bounds": None}
 
 
 def setup_pr2() -> PR2:
@@ -19,6 +20,13 @@ def setup_pr2() -> PR2:
         pr2.reset_manip_pose()
         _cache["pr2"] = pr2
     return _cache["pr2"]  # type: ignore
+
+
+def setup_bounds() -> Bounds:
+    if _cache["bounds"] is None:
+        bounds = PR2Paramter.rarm_default_bounds(with_base=True)
+        _cache["bounds"] = bounds  # type: ignore
+    return _cache["bounds"]  # type: ignore
 
 
 def setup_kinmaps() -> Tuple[
@@ -32,6 +40,12 @@ def setup_kinmaps() -> Tuple[
         colkin.reflect_skrobot_model(pr2)
         _cache["kinmap"] = (efkin, colkin)  # type: ignore
     return _cache["kinmap"]  # type: ignore
+
+
+def build_pr2_cache() -> None:
+    setup_pr2()
+    setup_bounds()
+    setup_kinmaps()
 
 
 def get_pr2_kinect_camera() -> Camera:
