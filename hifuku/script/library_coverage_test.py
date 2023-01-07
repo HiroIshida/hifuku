c2992224a75b1513e2453bd22447ba057a4497e4
HiroIshida <spitfire.docomo@gmail.com>
2023-01-07 15:35:18 +0900

    fix(library_coverage_test): use domain provider

--- hifuku/script/library_coverage_test.py
+++ hifuku/script/library_coverage_test.py
@@ -1,16 +1,20 @@
-from pathlib import Path
-
 import numpy as np
 import torch
+from mohou.file import get_project_path
 
-from hifuku.datagen import MultiProcessBatchProblemSolver
+from hifuku.domain import TBRR_SQP_DomainProvider
 from hifuku.library import SolutionLibrary
-from hifuku.threedim.tabletop import TabletopPlanningProblem
+from hifuku.rpbench_wrap import TabletopBoxRightArmReachingTask
 
 np.random.seed(0)
-p = Path("~/.mohou/tabletop_solution_library").expanduser()
-validation_pool = [TabletopPlanningProblem.sample(1) for _ in range(100)]
-lib = SolutionLibrary.load(p, TabletopPlanningProblem, device=torch.device("cpu"))[0]
-solver = MultiProcessBatchProblemSolver[TabletopPlanningProblem]()
-coverage = lib.measure_full_coverage(validation_pool, solver)
+
+task_type = TBRR_SQP_DomainProvider.get_task_type()
+solver_type = TBRR_SQP_DomainProvider.get_solver_type()
+mp_batch_solver = TBRR_SQP_DomainProvider.get_multiprocess_batch_solver()
+domain_name = TBRR_SQP_DomainProvider.get_domain_name()
+
+pp = get_project_path("tabletop_solution_library-{}".format(domain_name))
+validation_pool = [TabletopBoxRightArmReachingTask.sample(1) for _ in range(100)]
+lib = SolutionLibrary.load(pp, task_type, solver_type, device=torch.device("cpu"))[0]
+coverage = lib.measure_full_coverage(validation_pool, mp_batch_solver)
 print(coverage)
