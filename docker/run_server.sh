acc2f7a7727f91c2812d57f24109c2b03e46927f
HiroIshida <spitfire.docomo@gmail.com>
2023-01-08 03:36:37 +0900

    docker: update docker

--- docker/run_server.sh
+++ docker/run_server.sh
@@ -8,8 +8,11 @@ echo "port is set to $port"
 
 docker run --rm -it -p $port:8080 hifuku:latest \
     /bin/bash -i -c \
-    'cd ~/scikit-plan && git fetch origin && git checkout origin/master && pip3 install -e . ;\
+    '
     cd ~/scikit-robot && git remote add h-ishida git@github.com:HiroIshida/scikit-robot.git && git fetch h-ishida && git checkout h-ishida/master && pip3 install -e . ;\
+    cd ~/selcol && git fetch origin && git checkout origin/master && pip3 install -e . ;\
+    cd ~/scikit-motionplan && git fetch origin && git checkout origin/master && pip3 install -e . ;\
+    cd ~/rpbench && git fetch origin && git checkout origin/master && pip3 install -e . ;\
     cd ~/hifuku && git fetch origin && git checkout origin/master && pip3 install -e . ;\
     pip3 freeze ;\
     python3 -m hifuku.http_datagen.server'
