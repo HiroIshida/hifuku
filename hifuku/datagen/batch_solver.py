0bc5a97c47488abec2d7e4ac8f2a227ddc9f9d56
HiroIshida <spitfire.docomo@gmail.com>
2023-01-18 12:37:02 +0900

    refactor: remove useless functions

--- hifuku/datagen/batch_solver.py
+++ hifuku/datagen/batch_solver.py
@@ -23,27 +23,12 @@ from hifuku.datagen.http_datagen.request import (
     http_connection,
     send_request,
 )
+from hifuku.datagen.utils import split_indices
 from hifuku.pool import ProblemT
 from hifuku.types import RawData
 from hifuku.utils import filter_warnings
 
 logger = logging.getLogger(__name__)
-
-
-def split_number(num, div):
-    return [num // div + (1 if x < num % div else 0) for x in range(div)]
-
-
-def split_indices(n_problem_total: int, n_problem_list: List[int]) -> List[List[int]]:
-    indices = np.array(list(range(n_problem_total)))
-    indices_list = []
-    head = 0
-    for n_problem in n_problem_list:
-        tail = head + n_problem
-        indices_list.append(indices[head:tail].tolist())
-        head = tail
-    assert sum([len(ii) for ii in indices_list]) == n_problem_total
-    return indices_list
 
 
 @dataclass
