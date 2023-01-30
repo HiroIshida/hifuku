1428dbfb18c7b67565815ea06035f4e88228a31f
HiroIshida <spitfire.docomo@gmail.com>
2023-01-31 08:54:52 +0900

    fix: fp_rate = fp / (tp + fp)

--- hifuku/coverage.py
+++ hifuku/coverage.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from functools import cached_property
-from math import floor
 
 import numpy as np
 
@@ -43,21 +42,33 @@ class CoverageResult:
             self.values_ground_truth <= self.threshold, self.values_estimation > self.threshold
         )
 
-    def determine_margin(self, acceptable_fales_positive_rate: float) -> float:
+    def determine_margin(self, acceptable_false_positive_rate: float) -> float:
+        """
+        note that fp rate is defined as fp/(fp + tp)
+        """
         fp_bools = self.false_postive_bools
         values_est_fp = self.values_estimation[fp_bools]
         diffs = self.threshold - values_est_fp
         assert np.all(diffs > 0.0)
 
-        num_acceptable = floor(len(self) * acceptable_fales_positive_rate)
+        positive_gt = self.values_ground_truth <= self.threshold
 
-        n_fp = sum(fp_bools)
-        is_already_satisifed = n_fp <= num_acceptable
-        if is_already_satisifed:
+        def fp_rate(margin: float) -> float:
+            positive_est = self.values_estimation + margin <= self.threshold
+            tp_rate = np.sum(np.logical_and(positive_gt, positive_est)) / np.sum(positive_est)
+            fp_rate = 1.0 - tp_rate
+            return fp_rate
+
+        if fp_rate(0.0) < acceptable_false_positive_rate:
             return 0.0
 
-        sorted_diffs = np.sort(diffs)[::-1]  # decreasing order
-        return sorted_diffs[num_acceptable] + 1e-6
+        sorted_diffs = np.sort(diffs)
+        for i in range(len(diffs)):
+            margin_cand = sorted_diffs[i]
+            rate = fp_rate(margin_cand)
+            if rate < acceptable_false_positive_rate:
+                return margin_cand + 1e-5
+        assert False
 
     def __str__(self) -> str:
         string = "coverage result => "
--- tests/test_margin.py
+++ tests/test_margin.py
@@ -15,7 +15,7 @@ def test_coverage_result():
         def func_est(x: np.ndarray) -> np.ndarray:
             return (x - (0.5 + error)) + np.random.randn(len(x)) * 0.03
 
-        n_sample = 100
+        n_sample = 1000
         xs = np.random.rand(n_sample)
         values_gt = func_gt(xs)
         values_est = func_est(xs)
@@ -40,7 +40,11 @@ def test_coverage_result():
         assert n_sum == n_sample
 
         n_fp = sum(result.false_postive_bools)
-        n_fp_expected = min(floor(n_sample * rate_threshold), np.sum(result.false_postive_bools))
+
+        total_positive_num = sum(result.true_positive_bools) + sum(result.false_postive_bools)
+        n_fp_expected = min(
+            floor(total_positive_num * rate_threshold), np.sum(result.false_postive_bools)
+        )
         assert n_fp == n_fp_expected
 
 
