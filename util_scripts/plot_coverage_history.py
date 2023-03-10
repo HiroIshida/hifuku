ac8c819e8d95425c1d45f4df7f3448df4bbb0890
HiroIshida <spitfire.docomo@gmail.com>
2023-03-11 00:37:39 +0900

    feat: add function that computes the covering rate curve

--- util_scripts/plot_coverage_history.py
+++ util_scripts/plot_coverage_history.py
@@ -1,19 +1,20 @@
 import argparse
-from enum import Enum
 
 import matplotlib.pyplot as plt
 import numpy as np
-from rpbench.tabletop import (
-    TabletopBoxDualArmReachingTask,
-    TabletopBoxRightArmReachingTask,
-)
 
 from hifuku.script_utils import load_library
 
 
-class TaskSelector(Enum):
-    tbrr = TabletopBoxRightArmReachingTask
-    tbdr = TabletopBoxDualArmReachingTask
+def get_rate_history(lib):
+    n_coverage_sample = len(lib.coverage_results[0])
+    bools = np.zeros(n_coverage_sample)
+    rates = [0.0]
+    for covres, m in zip(lib.coverage_results, lib.margins):
+        est_ok = covres.values_estimation + m < covres.threshold
+        bools = np.logical_or(bools, est_ok)
+        rates.append(np.sum(bools) / n_coverage_sample)
+    return rates
 
 
 if __name__ == "__main__":
@@ -23,13 +24,7 @@ if __name__ == "__main__":
     domain_name: str = args.domain
 
     lib = load_library(domain_name, "cpu")
-    n_coverage_sample = len(lib.coverage_results[0])
-    bools = np.zeros(n_coverage_sample)
 
-    rates = [0.0]
-    for covres, m in zip(lib.coverage_results, lib.margins):
-        est_ok = covres.values_estimation + m < covres.threshold
-        bools = np.logical_or(bools, est_ok)
-        rates.append(np.sum(bools) / n_coverage_sample)
+    rates = get_rate_history(lib)
     plt.plot(rates)
     plt.show()
