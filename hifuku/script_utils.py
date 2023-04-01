b4f23628a761a53a75a2f1b97743af1c71108114
HiroIshida <spitfire.docomo@gmail.com>
2023-04-02 01:06:16 +0900

    feat: accept either domain_name or domain type

--- hifuku/script_utils.py
+++ hifuku/script_utils.py
@@ -1,17 +1,18 @@
 from pathlib import Path
-from typing import Literal, Optional
+from typing import Literal, Optional, Type, Union
 
 import mohou.file
 import torch
 from mohou.trainer import TrainCache
 
-from hifuku.domain import select_domain
+from hifuku.domain import DomainProtocol, select_domain
 from hifuku.library import SolutionLibrary
 from hifuku.neuralnet import AutoEncoderBase, NullAutoEncoder, VoxelAutoEncoder
 
 
-def load_compatible_autoencoder(domain_name: str) -> AutoEncoderBase:
-    domain = select_domain(domain_name)
+def load_compatible_autoencoder(domain: Union[str, Type[DomainProtocol]]) -> AutoEncoderBase:
+    if isinstance(domain, str):
+        domain = select_domain(domain)
     if domain.mesh_sampler_type is None:
         ae_model: AutoEncoderBase = NullAutoEncoder()
     else:
@@ -20,8 +21,11 @@ def load_compatible_autoencoder(domain_name: str) -> AutoEncoderBase:
     return ae_model
 
 
-def get_project_path(domain_name: str) -> Path:
-    domain = select_domain(domain_name)
+def get_project_path(domain: Union[str, Type[DomainProtocol]]) -> Path:
+
+    if isinstance(domain, str):
+        domain = select_domain(domain)
+
     domain_identifier = domain.get_domain_name()
     pp = mohou.file.get_project_path("tabletop_solution_library-{}".format(domain_identifier))
     pp.mkdir(exist_ok=True)
@@ -29,14 +33,17 @@ def get_project_path(domain_name: str) -> Path:
 
 
 def load_library(
-    domain_name: str,
+    domain: Union[str, Type[DomainProtocol]],
     device: Literal["cpu", "cuda"],
     limit_thread: bool = False,
     project_path: Optional[Path] = None,
 ) -> SolutionLibrary:
-    domain = select_domain(domain_name)
+
+    if isinstance(domain, str):
+        domain = select_domain(domain)
+
     if project_path is None:
-        project_path = get_project_path(domain_name)
+        project_path = get_project_path(domain)
     lib = SolutionLibrary.load(
         project_path, domain.task_type, domain.solver_type, torch.device(device)
     )[0]
