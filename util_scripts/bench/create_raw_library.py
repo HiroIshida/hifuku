338f11a8884591ce8f0c99146cf913dcf23af5cb
HiroIshida <spitfire.docomo@gmail.com>
2023-05-25 13:08:52 +0900

    example: scriptize create_raw_library

--- util_scripts/bench/create_raw_library.py
+++ util_scripts/bench/create_raw_library.py
@@ -1,5 +1,13 @@
+import argparse
+
 from rpbench.interface import PlanningDataset
 from rpbench.jaxon.below_table import HumanoidTableReachingTask
 
-dataset = PlanningDataset.create(HumanoidTableReachingTask, 10000, 12)
-dataset.save()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-domain", type=str, help="")
+    parser.add_argument("-n", type=int, default=300, help="")
+    parser.add_argument("-m", type=int, default=2, help="number of process")
+
+    dataset = PlanningDataset.create(HumanoidTableReachingTask, 10000, 12)
+    dataset.save()
