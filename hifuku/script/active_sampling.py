18b4846f9f4e4b5da70f8345bf87b66a444aa1c4
HiroIshida <spitfire.docomo@gmail.com>
2023-06-14 22:32:31 +0900

    tmp: active sampling

--- hifuku/script/active_sampling.py
+++ hifuku/script/active_sampling.py
@@ -19,6 +19,7 @@ from hifuku.utils import create_default_logger, filter_warnings
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-type", type=str, default="tbrr_sqp", help="")
+    parser.add_argument("-n", type=int, default=100, help="")
     parser.add_argument("--warm", action="store_true", help="warm start")
     parser.add_argument("--lm", action="store_true", help="use light weight nn")
     parser.add_argument("--fptest", action="store_true", help="test false positive rate")
@@ -28,12 +29,13 @@ if __name__ == "__main__":
     warm_start: bool = args.warm
     use_light_model: bool = args.lm
     test_fp_rate: bool = args.fptest
+    n_step: int = args.n
 
     filter_warnings()
     domain = select_domain(domain_name)
-    pp = get_project_path(domain_name)
+    project_path = get_project_path(domain_name)
 
-    logger = create_default_logger(pp, "library_gen")
+    logger = create_default_logger(project_path, "library_gen")
     log_package_version_info(logger, hifuku)
     log_package_version_info(logger, rpbench)
     log_package_version_info(logger, skmp)
@@ -45,17 +47,29 @@ if __name__ == "__main__":
         iterpred_model_config = None  # type: ignore
 
     lconfig = LibrarySamplerConfig(
-        n_problem=10000,
-        n_problem_inner=200,
+        n_problem=5000,
+        n_problem_inner=30,
         train_config=TrainConfig(n_epoch=100),
         n_solution_candidate=100,
-        n_difficult_problem=500,
+        n_difficult_problem=300,
         solvable_threshold_factor=1.0,
         difficult_threshold_factor=1.0,
-        acceptable_false_positive_rate=0.03,
+        acceptable_false_positive_rate=0.1,
         iterpred_model_config=iterpred_model_config,
         bootstrap_trial=1000,
     )  # all pass
+    # lconfig = LibrarySamplerConfig(
+    #     n_problem=2000,
+    #     n_problem_inner=20,
+    #     train_config=TrainConfig(n_epoch=30),
+    #     n_solution_candidate=30,
+    #     n_difficult_problem=100,
+    #     solvable_threshold_factor=1.0,
+    #     difficult_threshold_factor=1.0,
+    #     acceptable_false_positive_rate=0.1,
+    #     iterpred_model_config=iterpred_model_config,
+    #     bootstrap_trial=1000,
+    # )  # all pass
 
     ae_model = load_compatible_autoencoder(domain_name)
     lib_sampler = SimpleSolutionLibrarySampler.initialize(
@@ -64,6 +78,7 @@ if __name__ == "__main__":
         domain.solver_config,
         ae_model,
         lconfig,
+        project_path,
         pool_single=None,
         use_distributed=True,
         reuse_cached_validation_set=warm_start,
@@ -73,6 +88,6 @@ if __name__ == "__main__":
     if warm_start:
         lib_sampler.library = load_library(domain_name, "cuda", True)
 
-    for i in range(100):
+    for i in range(n_step):
         print(i)
-        lib_sampler.step_active_sampling(pp)
+        lib_sampler.step_active_sampling()
