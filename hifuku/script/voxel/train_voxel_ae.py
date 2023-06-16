449ec54fab577965faaf54457b6b06ad22368848
HiroIshida <spitfire.docomo@gmail.com>
2023-06-16 15:34:17 +0900

    feat: train voxel ae with --warm

--- hifuku/script/voxel/train_voxel_ae.py
+++ hifuku/script/voxel/train_voxel_ae.py
@@ -1,3 +1,5 @@
+import argparse
+
 from mohou.file import get_project_path
 from mohou.script_utils import create_default_logger
 from mohou.trainer import TrainCache, TrainConfig, train_lower
@@ -6,22 +8,32 @@ from hifuku.llazy.dataset import LazyDecomplessDataLoader, LazyDecomplessDataset
 from hifuku.neuralnet import AutoEncoderConfig, VoxelAutoEncoder
 from hifuku.rpbench_wrap import TabletopBoxWorldWrap
 
-problem_type = TabletopBoxWorldWrap
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--warm", action="store_true", help="warm start")
+    args = parser.parse_args()
+    warm_start: bool = args.warm
 
-pp = get_project_path("hifuku-{}".format(problem_type.__name__))
-cache_base_path = pp / "cache"
-cache_base_path.mkdir(exist_ok=True, parents=True)
+    problem_type = TabletopBoxWorldWrap
 
-logger = create_default_logger(pp, "voxel_ae")
+    pp = get_project_path("hifuku-{}".format(problem_type.__name__))
+    cache_base_path = pp / "cache"
+    cache_base_path.mkdir(exist_ok=True, parents=True)
 
-dataset = LazyDecomplessDataset.load(cache_base_path, problem_type, n_worker=-1)
-print("finish setup dataset")
-dataset_train, dataset_valid = dataset.random_split(0.1)
-train_loader = LazyDecomplessDataLoader(dataset_train, batch_size=500)
-valid_loader = LazyDecomplessDataLoader(dataset_valid, batch_size=50, shuffle=False)
-print("finish setup loader")
+    logger = create_default_logger(pp, "voxel_ae")
 
-model_conf = AutoEncoderConfig()
-tcache = TrainCache.from_model(VoxelAutoEncoder(model_conf))
-train_conf = TrainConfig(n_epoch=100)
-train_lower(pp, tcache, train_loader, valid_loader, train_conf)
+    dataset = LazyDecomplessDataset.load(cache_base_path, problem_type, n_worker=-1)
+    print("finish setup dataset")
+    dataset_train, dataset_valid = dataset.random_split(0.1)
+    train_loader = LazyDecomplessDataLoader(dataset_train, batch_size=500)
+    valid_loader = LazyDecomplessDataLoader(dataset_valid, batch_size=50, shuffle=False)
+    print("finish setup loader")
+
+    if warm_start:
+        tcache = TrainCache.load(pp, VoxelAutoEncoder)
+    else:
+        model_conf = AutoEncoderConfig()
+        tcache = TrainCache.from_model(VoxelAutoEncoder(model_conf))
+
+    train_conf = TrainConfig(n_epoch=100)
+    train_lower(pp, tcache, train_loader, valid_loader, train_conf)
