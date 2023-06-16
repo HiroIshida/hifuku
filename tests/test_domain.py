c05e0f366dea364dd5d3c2d163c6e11bd58ac867
HiroIshida <spitfire.docomo@gmail.com>
2023-06-17 03:16:24 +0900

    test: fix test_domain

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -2,4 +2,4 @@ from hifuku.domain import TORR_SQP_Domain
 
 
 def test_domain():
-    assert TORR_SQP_Domain.get_domain_name() == "TBRR_SQP"
+    assert TORR_SQP_Domain.get_domain_name() == "TORR_SQP"
