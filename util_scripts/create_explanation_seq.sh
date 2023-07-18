1f07b248e76c98bdecdb42d2d0ea8d003eb99fdd
HiroIshida <spitfire.docomo@gmail.com>
2023-07-19 04:10:22 +0900

    script: visualize dummy task

---
+++ util_scripts/create_explanation_seq.sh
@@ -0,0 +1,10 @@
+parallel ::: \
+"python3 visualize_2dtask_result.py --save -n 1 -margin all --active --legend" \
+"python3 visualize_2dtask_result.py --save -n 0 -margin all --active" \
+"python3 visualize_2dtask_result.py --save -n 1 -margin all --active" \
+"python3 visualize_2dtask_result.py --save -n 2 -margin all --active" \
+"python3 visualize_2dtask_result.py --save -n 3 -margin all --active" \
+"python3 visualize_2dtask_result.py --save -n 4 -margin all --active" \
+"python3 visualize_2dtask_result.py --save -n 5 -margin all --active" \
+"python3 visualize_2dtask_result.py --save -n 6 -margin all --active" \
+"python3 visualize_2dtask_result.py --save -n 7 -margin all --active" \
--- util_scripts/visualize_2dtask_result.py
+++ util_scripts/visualize_2dtask_result.py
@@ -1,12 +1,36 @@
+import argparse
+import pickle
+from pathlib import Path
+from typing import Literal, Optional
+
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-from hifuku.domain import EightRooms_SQP_Domain
+from hifuku.domain import DummyDomain
 from hifuku.script_utils import load_library
 
-domain = EightRooms_SQP_Domain
+
+def sample_colors(n_colors, cmap_name="hsv"):
+    cmap = plt.cm.get_cmap(cmap_name)  # get the colormap
+    colors = cmap(np.linspace(0, 1, n_colors))  # sample the colormap
+    return colors
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", type=int)
+parser.add_argument("-margin", type=str, default="default")
+parser.add_argument("--active", action="store_true")
+parser.add_argument("--legend", action="store_true")
+parser.add_argument("--save", action="store_true")
+args = parser.parse_args()
+n_step: Optional[int] = args.n
+margin_mode: Literal["default", "latest", "all"] = args.margin
+show_active_sampling: bool = args.active
+
+# domain = EightRooms_SQP_Domain
 # domain = EightRooms_Lightning_Domain
+domain = DummyDomain
 config = domain.solver_config
 world = domain.task_type.get_world_type().sample()
 
@@ -16,36 +40,103 @@ fig, ax = plt.subplots()
 world.visualize((fig, ax))
 
 sdf = world.get_exact_sdf()
-cmap = plt.cm.get_cmap(plt.cm.viridis, 143)
+sampled_colors = sample_colors(8)
+print(len(lib.predictors))
 
-for pred, margin in zip(lib.predictors, lib.margins):
+if n_step > 0:
 
-    sol = pred.initial_solution
-    assert sol is not None
-    arr = sol.numpy()
+    margins_history = lib._margins_history
+    margins_at_time = margins_history[n_step - 1]
+    for i in range(n_step):
+        is_latest = i == n_step - 1
 
-    xlin = np.linspace(-1, 1, 100)
-    ylin = np.linspace(-1, 1, 100)
-    X, Y = np.meshgrid(xlin, ylin)
-    pts = list(zip(X.flatten(), Y.flatten()))
-    n_batch = len(pts)
-    for i in range(n_batch):
-        pts[i] = np.hstack((np.array([0.0, 0.0]), pts[i]))
-    pts = np.array(pts)
-    pts_torch = torch.from_numpy(np.array(pts)).float()
+        pred, margin = lib.predictors[i], margins_at_time[i]
 
-    mesh_torch = torch.empty((n_batch, 0))
-    iter_preds, _ = pred.forward((mesh_torch, pts_torch))
-    print(config.n_max_call)
-    iters = iter_preds.cpu().detach().numpy().flatten() - (config.n_max_call - margin)
+        sol = pred.initial_solution
+        assert sol is not None
+        arr = sol.numpy()
 
-    values = -sdf(pts[:, 2:])
-    iters = np.maximum(values, iters)
-    iters = iters.reshape(100, 100)
+        n_grid = 600
+        xlin = np.linspace(-2.2, 2.2, n_grid)
+        ylin = np.linspace(-2.2, 2.2, n_grid)
+        X, Y = np.meshgrid(xlin, ylin)
+        pts = list(zip(X.flatten(), Y.flatten()))
+        n_batch = len(pts)
+        if domain.task_type.get_dof() != 2:
+            for i in range(n_batch):
+                pts[i] = np.hstack((np.array([0.0, 0.0]), pts[i]))
+        pts = np.array(pts)
+        pts_torch = torch.from_numpy(np.array(pts)).float()
 
-    c = np.random.rand(3)
-    ax.contourf(X, Y, iters, levels=[-np.inf, 0], colors=[c], alpha=0.4)
+        mesh_torch = torch.empty((n_batch, 0))
+        iter_preds, _ = pred.forward((mesh_torch, pts_torch))
 
-    ax.plot(arr[:, 0], arr[:, 1], color=c)
-    ax.scatter(arr[-1, 0], arr[-1, 1], color="black")
-plt.show()
+        iters_main = iter_preds.cpu().detach().numpy().flatten() - (config.n_max_call - margin)
+        assert np.any(iters_main > 0)
+
+        if domain.task_type.get_dof() == 2:
+            values = -sdf(pts)
+        else:
+            values = -sdf(pts[:, 2:])
+        iters_main = np.maximum(values, iters_main)
+        iters_main = iters_main.reshape(n_grid, n_grid)
+
+        c = sampled_colors[i]
+        if is_latest:
+            ax.contourf(X, Y, iters_main, levels=[-np.inf, 0], colors=[c], alpha=0.9)
+            CS_main = ax.contour(X, Y, iters_main, levels=[0], colors=["black"], linewidths=2.5)
+            CS_main.collections[0].set_label("translated (latest)")
+            ax.scatter(arr[-1, 0], arr[-1, 1], color="black", label="guiding trajectory")
+        else:
+            ax.contourf(X, Y, iters_main, levels=[-np.inf, 0], colors=[c], alpha=0.2)
+            CS_main = ax.contour(X, Y, iters_main, levels=[0], colors=["black"])
+            CS_main.collections[0].set_label("translated")
+            ax.scatter(arr[-1, 0], arr[-1, 1], color="gray", label="guiding trajectory")
+
+        # plot raw boundary
+        if (margin_mode == "all") or (margin_mode == "latest" and is_latest):
+            iters_raw = iter_preds.cpu().detach().numpy().flatten() - config.n_max_call
+            if domain.task_type.get_dof() == 2:
+                values = -sdf(pts)
+            else:
+                values = -sdf(pts[:, 2:])
+            iters_raw = np.maximum(values, iters_raw)
+            iters_raw = iters_raw.reshape(n_grid, n_grid)
+
+            if is_latest:
+                CS_raw = ax.contour(X, Y, iters_raw, levels=[0], colors=["red"], linewidths=2.5)
+                CS_raw.collections[0].set_label("raw (latest)")
+            else:
+                CS_raw = ax.contour(
+                    X, Y, iters_raw, levels=[0], colors=["red"], linestyles="dashed"
+                )
+                CS_raw.collections[0].set_label("raw")
+
+if show_active_sampling:
+    p = Path("/tmp/hifuku-debug-data/feasible_solutions-{}.pkl".format(n_step))
+    if p.exists():
+        with p.open(mode="rb") as f:
+            solutions = pickle.load(f)
+        pts = []
+        for traj in solutions:
+            pts.append(traj.numpy()[-1])
+        pts = np.array(pts)
+        ax.scatter(pts[:, 0], pts[:, 1], c="b", s=1, label="candidates")
+    if n_step < len(lib.predictors):
+        p_selected = lib.predictors[n_step].initial_solution.numpy()[-1]
+        ax.scatter(p_selected[0], p_selected[1], c="orange", marker="*", s=70, label="selected")
+
+ax.set_xlim(-1.0, 2.2)
+ax.set_ylim(-1.8, 1.6)
+if args.legend:
+    legend = ax.legend()
+    legend.get_frame().set_alpha(1.0)
+    if args.save:
+        plt.savefig("./figs/caption-algo-expl-seq-{}.png".format(n_step), dpi=300)
+    else:
+        plt.show()
+else:
+    if args.save:
+        plt.savefig("./figs/algo-expl-seq-{}.png".format(n_step), dpi=300)
+    else:
+        plt.show()
