83018e82cf1feed79673b99c4bbb0490109fbd0b
HiroIshida <spitfire.docomo@gmail.com>
2023-07-19 04:10:22 +0900

    script: visualize dummy task

--- util_scripts/visualize_2dtask_result.py
+++ util_scripts/visualize_2dtask_result.py
@@ -1,12 +1,16 @@
+import pickle
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-from hifuku.domain import EightRooms_SQP_Domain
+from hifuku.domain import DummyDomain
 from hifuku.script_utils import load_library
 
-domain = EightRooms_SQP_Domain
+# domain = EightRooms_SQP_Domain
 # domain = EightRooms_Lightning_Domain
+domain = DummyDomain
 config = domain.solver_config
 world = domain.task_type.get_world_type().sample()
 
@@ -18,19 +22,25 @@ world.visualize((fig, ax))
 sdf = world.get_exact_sdf()
 cmap = plt.cm.get_cmap(plt.cm.viridis, 143)
 
-for pred, margin in zip(lib.predictors, lib.margins):
+n_step = 4
+
+margins_at_time = lib._margins_history[n_step-1]
+for i in range(n_step):
+    pred, margin = lib.predictors[i], margins_at_time[i]
 
     sol = pred.initial_solution
     assert sol is not None
     arr = sol.numpy()
 
-    xlin = np.linspace(-1, 1, 100)
-    ylin = np.linspace(-1, 1, 100)
+    n_grid = 400
+    xlin = np.linspace(-2.2, 2.2, n_grid)
+    ylin = np.linspace(-2.2, 2.2, n_grid)
     X, Y = np.meshgrid(xlin, ylin)
     pts = list(zip(X.flatten(), Y.flatten()))
     n_batch = len(pts)
-    for i in range(n_batch):
-        pts[i] = np.hstack((np.array([0.0, 0.0]), pts[i]))
+    if domain.task_type.get_dof() != 2:
+        for i in range(n_batch):
+            pts[i] = np.hstack((np.array([0.0, 0.0]), pts[i]))
     pts = np.array(pts)
     pts_torch = torch.from_numpy(np.array(pts)).float()
 
@@ -39,13 +49,31 @@ for pred, margin in zip(lib.predictors, lib.margins):
     print(config.n_max_call)
     iters = iter_preds.cpu().detach().numpy().flatten() - (config.n_max_call - margin)
 
-    values = -sdf(pts[:, 2:])
+    if domain.task_type.get_dof() == 2:
+        values = -sdf(pts)
+    else:
+        values = -sdf(pts[:, 2:])
     iters = np.maximum(values, iters)
-    iters = iters.reshape(100, 100)
+    iters = iters.reshape(n_grid, n_grid)
 
     c = np.random.rand(3)
     ax.contourf(X, Y, iters, levels=[-np.inf, 0], colors=[c], alpha=0.4)
+    # ax.contour(X, Y, iters, levels=[-margin], colors=["red"])
+    ax.contour(X, Y, iters, levels=[0.1], colors=["black"])
 
-    ax.plot(arr[:, 0], arr[:, 1], color=c)
+    # ax.plot(arr[:, 0], arr[:, 1], color=c)
     ax.scatter(arr[-1, 0], arr[-1, 1], color="black")
+    ax.set_xlim(-1.0, 2.2)
+    ax.set_ylim(-1.8, 1.6)
+
+with Path("/tmp/hifuku-debug-data/feasible_solutions-{}.pkl".format(n_step)).open(mode="rb") as f:
+    solutions = pickle.load(f)
+
+pts = []
+for traj in solutions:
+    pts.append(traj.numpy()[-1])
+pts = np.array(pts)
+ax.scatter(pts[:, 0], pts[:, 1])
+
+
 plt.show()
