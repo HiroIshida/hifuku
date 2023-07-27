5bd019ebfabf42c5301080a81c36579da3ad876e
HiroIshida <spitfire.docomo@gmail.com>
2023-07-28 05:55:09 +0900

    script: update run_benchmark

--- util_scripts/bench/run_benchmark.py
+++ util_scripts/bench/run_benchmark.py
@@ -10,6 +10,7 @@ from rpbench.interface import PlanningDataset
 
 from hifuku.domain import select_domain
 from hifuku.library import LibraryBasedGuaranteedSolver, SolutionLibrary
+from hifuku.utils import create_default_logger, filter_warnings
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -17,6 +18,7 @@ if __name__ == "__main__":
     parser.add_argument("-n", type=int, default=300, help="")
     parser.add_argument("--feasible", action="store_true", help="use only feasible problem set")
     parser.add_argument("--dataset", action="store_true", help="load dataset")
+    parser.add_argument("--proposed", action="store_true", help="use only proposed")
     args = parser.parse_args()
 
     n_sample = args.n
@@ -27,8 +29,13 @@ if __name__ == "__main__":
     pp = get_project_path(domain_name)
     task_type = domain.task_type
 
+    filter_warnings()
+
+    logger = create_default_logger(Path("./").expanduser(), "run_benchmark")
+    logger.info(args)
+
     if only_feasible:
-        print("use feasible problem set")
+        logger.info("use feasible problem set")
         problem_set_path = Path("./problem_set") / (task_type.__name__ + ".pkl")
         with problem_set_path.open(mode="rb") as f:
             pairs = pickle.load(f)
@@ -38,6 +45,7 @@ if __name__ == "__main__":
         assert n_sample <= len(tasks_full), "available: {}".format(len(tasks_full))
         tasks = tasks_full[:n_sample]
     else:
+        logger.info("sample problem set now")
         tasks = [task_type.sample(1) for _ in range(n_sample)]
     assert tasks[0].n_inner_task == 1
 
@@ -51,48 +59,51 @@ if __name__ == "__main__":
         dataset = PlanningDataset(pairs, task_type, time.time())
     else:
         dataset = None
-    solver_table = CompatibleSolvers.get_compatible_solvers(task_type.__name__, dataset=dataset)
-    # solver_table = {}
+    if args.proposed:
+        solver_table = {}
+    else:
+        solver_table = CompatibleSolvers.get_compatible_solvers(task_type.__name__, dataset=dataset)
 
     # setup proposed solver
     pp = get_project_path("hifuku-{}".format(domain.get_domain_name()))
     libraries = SolutionLibrary.load(pp, domain.task_type, domain.solver_type, torch.device("cpu"))
     lib = libraries[0]
     proposed = LibraryBasedGuaranteedSolver.init(lib)
+    proposed_name = "proposed-guaranteed"
     solver_table["proposed-guaranteed"] = proposed
 
-    # proposed = LibraryBasedHeuristicSolver.init(lib)
-    # solver_table["proposed-heuristic"] = proposed
-
     results = []
-    false_positive_seq = []
+    fp_count = 0
+    count_dict = {key: 0 for key in solver_table.keys()}
+    tasks_dump = []
 
     for i, task in enumerate(tasks):
-        print(i)
+        logger.info("task num: {}".format(i))
 
         result = {}
         for name, solver in solver_table.items():
-            print("solver name: {}".format(name))
-
-            print("start setting up")
+            logger.info("solver name: {}".format(name))
             solver.setup(task)
+            res = solver.solve()
+            logger.info("solved?: {}, time: {}".format(res.traj is not None, res.time_elapsed))
 
-            print("start solving")
-            res = solver.solve(timeout=10)
-            print("solved?: {}, time: {}".format(res.traj is not None, res.time_elapsed))
+            if res.traj is not None:
+                count_dict[name] += 1
 
             result[name] = res
 
-            if name == "proposed":
-                if solver.previous_false_positive is not None:
-                    false_positive_seq.append(solver.previous_false_positive)
-                    false_positive_count = sum(false_positive_seq)
-                    fp_rate = false_positive_count / len(false_positive_seq)
-                    print("current fp rate: {}".format(fp_rate))
+            if name == proposed_name:
+                if res.time_elapsed is not None and res.traj is None:
+                    fp_count += 1
 
+        logger.info("count dict: {}".format(count_dict))
+        tasks_dump.append(task)
         results.append(result)
+
+        fp_rate = fp_count / (i + 1)
+        logger.info("current fp rate: {}".format(fp_rate))
 
     result_base_path = Path("./result")
     result_path: Path = result_base_path / "result-{}".format(domain.get_domain_name())
     with result_path.open(mode="wb") as f:
-        pickle.dump((results, false_positive_seq), f)
+        pickle.dump((tasks_dump, results), f)
