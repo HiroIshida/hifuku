11999037cd1495a79d69f1b625267020d53825b0
HiroIshida <spitfire.docomo@gmail.com>
2023-08-31 21:47:19 +0900

    feat: add humanoid_tcrr2_sqp domain

--- hifuku/domain.py
+++ hifuku/domain.py
@@ -5,6 +5,7 @@ from typing import ClassVar, Optional, Protocol, Type
 import tqdm
 from rpbench.articulated.jaxon.below_table import (
     HumanoidTableClutteredReachingTask,
+    HumanoidTableClutteredReachingTask2,
     HumanoidTableReachingTask,
 )
 from rpbench.articulated.jaxon.ground import (
@@ -336,6 +337,22 @@ class HumanoidTableClutteredRarmReaching_SQP_Domain(DomainProtocol):
     auto_encoder_type = PixelAutoEncoder
 
 
+class HumanoidTableClutteredRarmReaching2_SQP_Domain(DomainProtocol):
+    task_type = HumanoidTableClutteredReachingTask2
+    solver_type = SQPBasedSolver
+    solver_config = SQPBasedSolverConfig(
+        n_wp=40,
+        n_max_call=5,
+        motion_step_satisfaction="explicit",
+        verbose=False,
+        ctol_eq=1e-3,
+        ctol_ineq=1e-3,
+        ineq_tighten_coef=0.0,
+    )
+    auto_encoder_project_name = "BelowTableClutteredWorld-AutoEncoder"
+    auto_encoder_type = PixelAutoEncoder
+
+
 class HumanoidGroundRarmReaching_SQP_Domain(DomainProtocol):
     task_type = HumanoidGroundRarmReachingTask
     solver_type = SQPBasedSolver
@@ -450,6 +467,7 @@ def select_domain(domain_name: str) -> Type[DomainProtocol]:
         eight_rooms_lt = EightRooms_LT_Domain
         humanoid_trr_sqp = HumanoidTableRarmReaching_SQP_Domain
         humanoid_tcrr_sqp = HumanoidTableClutteredRarmReaching_SQP_Domain
+        humanoid_tcrr2_sqp = HumanoidTableClutteredRarmReaching2_SQP_Domain
         humanoid_grr_sqp = HumanoidGroundRarmReaching_SQP_Domain
         humanoid_gtrr_sqp = HumanoidGroundTableRarmReaching_SQP_Domain
         bubbly_simple_mesh_sqp = BubblySimpleMeshPointConnecting_SQP_Domain
