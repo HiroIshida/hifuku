c815de501f73e49bb075573bb296ff7e2b802540
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2023-12-17 03:31:58 +0900

    script: update train_pixel_ae

--- hifuku/script/pixel/train_pixel_ae.py
+++ hifuku/script/pixel/train_pixel_ae.py
@@ -7,6 +7,7 @@ import numpy as np
 import torch
 import tqdm
 from mohou.file import get_project_path
+from mohou.script_utils import create_default_logger
 from mohou.trainer import TrainCache, TrainConfig, train
 from torch.utils.data import Dataset
 
@@ -30,7 +31,9 @@ class MyDataset(Dataset):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cache", action="store_true", help="use cache")
-    parser.add_argument("-domain", type=str, help="domain name")
+    parser.add_argument("--domain", type=str, help="domain name")
+    parser.add_argument("--sample", type=int, help="sample size", default=20000)
+    parser.add_argument("--bottleneck", type=int, help="bottleneck size", default=200)
     args = parser.parse_args()
     use_cache: bool = args.cache
     domain_name: str = args.domain
@@ -44,25 +47,28 @@ if __name__ == "__main__":
     path.mkdir(exist_ok=True, parents=True)
     cache_path = path / "mat_list_cache.pkl"
 
+    logger = create_default_logger(path, "pixel_ae")
+
     if use_cache:
         with cache_path.open(mode="rb") as rf:
             mat_list = pickle.load(rf)
     else:
         pool = TrivialProblemPool(task_type, 1).as_predicated()
         sampler = MultiProcessBatchProblemSampler()  # type: ignore[var-annotated]
-        problems = sampler.sample_batch(100000, pool)
+        problems = sampler.sample_batch(args.sample, pool)
 
         mat_list = []
         for prob in tqdm.tqdm(problems):
-            gridsdf = prob.cache
-            mat = np.expand_dims(gridsdf.values.reshape(gridsdf.grid.sizes).T, axis=0)
+            # gridsdf = prob.cache
+            # mat = np.expand_dims(gridsdf.values.reshape(gridsdf.grid.sizes).T, axis=0)
+            mat = np.expand_dims(prob.world.heightmap(), axis=0)
             mat_list.append(mat)
 
         with cache_path.open(mode="wb") as wf:
             pickle.dump(mat_list, wf)
 
     dataset = MyDataset(mat_list)
-    model = PixelAutoEncoder(AutoEncoderConfig())
+    model = PixelAutoEncoder(AutoEncoderConfig(n_grid=56, dim_bottleneck=args.bottleneck))
     tcache = TrainCache.from_model(model)
-    config = TrainConfig(n_epoch=300)
+    config = TrainConfig(n_epoch=2000)
     train(path, tcache, dataset)
