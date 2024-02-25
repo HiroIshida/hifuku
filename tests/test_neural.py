ed8597e60401938497005096c8e3d260a0f287fa
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-02-26 07:48:30 +0900

    fix/test(test_neural): update

--- tests/test_neural.py
+++ tests/test_neural.py
@@ -1,7 +1,8 @@
+from functools import lru_cache
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-import pytest
+import numpy as np
 import torch
 from mohou.trainer import TrainCache, TrainConfig, train
 
@@ -52,8 +53,8 @@ def test_network():
         model.loss(sample)
 
 
-@pytest.fixture(autouse=True, scope="module")
-def sol_tasks_and_resultss():
+@lru_cache(maxsize=1)
+def get_sol_tasks_and_resultss():
     domain = DoubleIntegratorBubblySimple_SQP
     task_type = domain.task_type
 
@@ -69,24 +70,29 @@ def sol_tasks_and_resultss():
 
     n_task = 20
     n_inner = 3
-    tasks = [task_type.sample(n_inner) for _ in range(n_task)]
-    batch_solver = MultiProcessBatchProblemSolver(domain.solver_type, domain.solver_config)
-    resultss = batch_solver.solve_batch(tasks, [sol.traj] * n_task)
-    return sol, tasks, resultss
+    task_paramss = np.array(
+        [task_type.sample(n_inner).to_intrinsic_desc_vecs() for _ in range(n_task)]
+    )
+    batch_solver = MultiProcessBatchProblemSolver(
+        domain.solver_type, domain.solver_config, task_type
+    )
+    resultss = batch_solver.solve_batch(task_paramss, [sol.traj] * n_task)
+    return sol, task_paramss, resultss
 
 
-def test_dataset(sol_tasks_and_resultss):
+def test_dataset():
+    sol_tasks_and_resultss = get_sol_tasks_and_resultss()
     domain = DoubleIntegratorBubblySimple_SQP
     device = torch.device("cpu")
     ae_config = AutoEncoderConfig(n_grid=56)
     ae = PixelAutoEncoder(ae_config, device=device)
-    sol, tasks, resultss = sol_tasks_and_resultss
+    sol, task_paramss, resultss = sol_tasks_and_resultss
 
-    n_data = len(tasks) * tasks[0].n_inner_task
+    n_data = len(task_paramss) * len(task_paramss[0])
 
     for use_weight in [True, False]:
         if use_weight:
-            weightss = torch.ones(len(tasks), tasks[0].n_inner_task) * 2.0
+            weightss = torch.ones(len(task_paramss), len(task_paramss[0])) * 2.0
             w_expected = 2.0
         else:
             weightss = None
@@ -94,7 +100,7 @@ def test_dataset(sol_tasks_and_resultss):
 
         # test dataset when ae is specified (encoded)
         dataset = IterationPredictorDataset.construct_from_paramss_and_resultss(
-            sol.traj, tasks, resultss, domain.solver_config, weightss, ae
+            sol.traj, task_paramss, resultss, domain.solver_config, domain.task_type, weightss, ae
         )
         assert len(dataset) == n_data
         assert dataset.n_inner == 3
@@ -116,7 +122,7 @@ def test_dataset(sol_tasks_and_resultss):
 
         # test dataset when ae is not specified
         dataset = IterationPredictorDataset.construct_from_paramss_and_resultss(
-            sol.traj, tasks, resultss, domain.solver_config, weightss, None
+            sol.traj, task_paramss, resultss, domain.solver_config, domain.task_type, weightss, None
         )
         assert len(dataset) == n_data
         assert dataset.n_inner == 3
@@ -137,19 +143,20 @@ def test_dataset(sol_tasks_and_resultss):
         assert w.item() == w_expected
 
 
-def test_training(sol_tasks_and_resultss):
+def test_training():
+    sol_tasks_and_resultss = get_sol_tasks_and_resultss()
     domain = DoubleIntegratorBubblySimple_SQP
     device = torch.device("cpu")
     ae_config = AutoEncoderConfig(n_grid=56)
     ae = PixelAutoEncoder(ae_config, device=device)
-    sol, tasks, resultss = sol_tasks_and_resultss
+    sol, task_paramss, resultss = sol_tasks_and_resultss
 
     train_config = TrainConfig(5, n_epoch=2)
-    n_dof_desc = 4
+    n_dof_desc = 2
 
     # test dataset when ae is specified (encoded)
     dataset = IterationPredictorDataset.construct_from_paramss_and_resultss(
-        sol.traj, tasks, resultss, domain.solver_config, None, ae
+        sol.traj, task_paramss, resultss, domain.solver_config, domain.task_type, None, ae
     )
 
     conf = IterationPredictorConfig(n_dof_desc, ae_config.dim_bottleneck, (10, 10, 10))
@@ -162,7 +169,7 @@ def test_training(sol_tasks_and_resultss):
 
     # test dataset when ae is not specified
     dataset_raw = IterationPredictorDataset.construct_from_paramss_and_resultss(
-        sol.traj, tasks, resultss, domain.solver_config, None, None
+        sol.traj, task_paramss, resultss, domain.solver_config, domain.task_type, None, None
     )
     conf = IterationPredictorWithEncoderConfig(iterpred_model, ae)
     combined_model = IterationPredictorWithEncoder(conf, device=device)
@@ -174,5 +181,5 @@ def test_training(sol_tasks_and_resultss):
 
 
 if __name__ == "__main__":
+    test_dataset()
     # test_training(sol_tasks_and_resultss())
-    pass
