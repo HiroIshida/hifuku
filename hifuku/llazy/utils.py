cd6064cd396b3dd7b9a43c5f8e67ff560ebfda14
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-02-27 21:07:28 +0900

    refactor: remove unmaintained modules

--- hifuku/llazy/utils.py
+++
@@ -1,5 +0,0 @@
-from typing import List
-
-
-def split_number(num: int, div: int) -> List[int]:
-    return [num // div + (1 if x < num % div else 0) for x in range(div)]
--- tests/test_domain.py
+++
@@ -1,5 +0,0 @@
-from hifuku.domain import TORR_SQP_Domain
-
-
-def test_domain():
-    assert TORR_SQP_Domain.get_domain_name() == "TORR_SQP"
