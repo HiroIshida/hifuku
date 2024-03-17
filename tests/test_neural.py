8bf3b557ed11a058fb6475ed13c44014fd9859c5
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-03-18 07:22:05 +0900

    fix/test(neural): update test

--- tests/test_neural.py
+++ tests/test_neural.py
@@ -17,7 +17,7 @@ from hifuku.neuralnet import (
     CostPredictorWithEncoderConfig,
     PixelAutoEncoder,
     VoxelAutoEncoder,
-    create_dataset_from_params_and_results,
+    _create_dataset_from_params_and_results,
 )
 
 
@@ -53,13 +53,13 @@ def test_network():
 
 
 @lru_cache(maxsize=None)
-def get_sol_tasks_and_resultss(domain):
+def get_sol_tasks_and_results(domain):
     task_type = domain.task_type
-    task_default = task_type.sample(1, True)
+    task_default = task_type.sample()
     # get a valid solution until it is found
     trial_count = 0
     while True:
-        sol = task_default.solve_default()[0]
+        sol = task_default.solve_default()
         if sol.traj is not None:
             break
         trial_count += 1
@@ -67,29 +67,28 @@ def get_sol_tasks_and_resultss(domain):
             assert False
 
     n_task = 20
-    n_inner = 3
-    task_paramss = np.array([task_type.sample(n_inner).to_task_params() for _ in range(n_task)])
+    task_params = np.array([task_type.sample().to_task_param() for _ in range(n_task)])
     batch_solver = MultiProcessBatchTaskSolver(domain.solver_type, domain.solver_config, task_type)
-    resultss = batch_solver.solve_batch(task_paramss, [sol.traj] * n_task)
-    return sol, task_paramss, resultss
+    results = batch_solver.solve_batch(task_params, [sol.traj] * n_task)
+    return sol, task_params, results
 
 
 def _test_dataset(domain, use_weight: bool, encode_image: bool):
     # for domain in [DummyMeshDomain, DummyDomain]:
-    sol_tasks_and_resultss = get_sol_tasks_and_resultss(domain)
+    sol_tasks_and_results = get_sol_tasks_and_results(domain)
     device = torch.device("cpu")
     if domain == DummyMeshDomain:
         ae_config = AutoEncoderConfig(n_grid=56)
         ae = PixelAutoEncoder(ae_config, device=device)
     else:
         ae = None
-    sol, task_paramss, resultss = sol_tasks_and_resultss
+    sol, task_params, results = sol_tasks_and_results
 
-    n_data = len(task_paramss) * len(task_paramss[0])
+    n_data = len(task_params)
 
     # for use_weight in [True, False]:
     if use_weight:
-        weightss = torch.ones(len(task_paramss), len(task_paramss[0])) * 2.0
+        weightss = torch.ones(n_data) * 2.0
         w_expected = 2.0
     else:
         weightss = None
@@ -97,15 +96,14 @@ def _test_dataset(domain, use_weight: bool, encode_image: bool):
 
     # for encode_image in [True, False]:
     if encode_image:
-        dataset = create_dataset_from_params_and_results(
-            task_paramss, resultss, domain.solver_config, domain.task_type, weightss, ae
+        dataset = _create_dataset_from_params_and_results(
+            task_params, results, domain.solver_config, domain.task_type, weightss, ae
         )
     else:
-        dataset = create_dataset_from_params_and_results(
-            task_paramss, resultss, domain.solver_config, domain.task_type, weightss, None
+        dataset = _create_dataset_from_params_and_results(
+            task_params, results, domain.solver_config, domain.task_type, weightss, None
         )
     assert len(dataset) == n_data
-    assert dataset.n_inner == 3
     mesh, desc, it, w = dataset[0]
     if domain == DummyMeshDomain:
         assert mesh.ndim == 1 if encode_image else 3
@@ -148,11 +146,11 @@ def test_dataset(domain, use_weight, encode_image):
 
 def _test_training(domain, use_pretrained_ae: bool):
     domain = DummyMeshDomain
-    sol_tasks_and_resultss = get_sol_tasks_and_resultss(domain)
+    sol_tasks_and_results = get_sol_tasks_and_results(domain)
     device = torch.device("cpu")
     ae_config = AutoEncoderConfig(n_grid=56)
     ae = PixelAutoEncoder(ae_config, device=device)
-    sol, task_paramss, resultss = sol_tasks_and_resultss
+    sol, task_params, results = sol_tasks_and_results
 
     train_config = TrainConfig(5, n_epoch=2)
     n_dof_desc = 2
@@ -161,13 +159,13 @@ def _test_training(domain, use_pretrained_ae: bool):
     costpred_model = CostPredictor(conf, device=device)
 
     if use_pretrained_ae:
-        dataset = create_dataset_from_params_and_results(
-            task_paramss, resultss, domain.solver_config, domain.task_type, None, ae
+        dataset = _create_dataset_from_params_and_results(
+            task_params, results, domain.solver_config, domain.task_type, None, ae
         )
         model = costpred_model
     else:
-        dataset = create_dataset_from_params_and_results(
-            task_paramss, resultss, domain.solver_config, domain.task_type, None, None
+        dataset = _create_dataset_from_params_and_results(
+            task_params, results, domain.solver_config, domain.task_type, None, None
         )
         conf = CostPredictorWithEncoderConfig(costpred_model, ae)
         model = CostPredictorWithEncoder(conf, device=device)
