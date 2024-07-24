f317b0b532ab5b876b5792ef4452d73de9e12870
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-07-25 02:30:22 +0900

    script: update voxel trainer (actually modified before April)

--- hifuku/script/train_voxel_autoencoder.py
+++ hifuku/script/train_voxel_autoencoder.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
-from pathlib import Path
 
 import numpy as np
 import threadpoolctl
 import torch
+from mohou.file import get_project_path
 from mohou.trainer import TrainCache, TrainConfig, train
 from rpbench.articulated.pr2.minifridge import PR2MiniFridgeTask
 from torch.utils.data import Dataset
 
-from hifuku.domain import PR2MiniFridge_RRT2000
+from hifuku.domain import PR2MiniFridgeVoxel_RRT2000
 from hifuku.neuralnet import VoxelAutoEncoder, VoxelAutoEncoderConfig
 from hifuku.pool import TaskPool
 from hifuku.script_utils import create_default_logger
@@ -20,7 +20,7 @@ class MyDataset(Dataset):
 
     def __init__(self, n_sample: int, distributed: bool = False):
         # sample tasks beforehand as it is too time-consuming to be done in the training loop
-        domain = PR2MiniFridge_RRT2000()
+        domain = PR2MiniFridgeVoxel_RRT2000
         if distributed:
             sampler = domain.get_distributed_batch_sampler()
         else:
@@ -43,17 +43,15 @@ class MyDataset(Dataset):
 
 if __name__ == "__main__":
     dataset = MyDataset(n_sample=50000, distributed=True)
-
-    model = VoxelAutoEncoder(VoxelAutoEncoderConfig())
-    tcache = TrainCache.from_model(model)
-    tconfig = TrainConfig(n_epoch=300)
-
-    pp = Path("./voxel_autoencoder")
+    domain = PR2MiniFridgeVoxel_RRT2000
+    pp = get_project_path(domain.auto_encoder_project_name)
+    warm = True
+    if warm:
+        tcache = TrainCache.load_latest(pp, VoxelAutoEncoder)
+        tconfig = TrainConfig(n_epoch=150)
+    else:
+        model = VoxelAutoEncoder(VoxelAutoEncoderConfig())
+        tcache = TrainCache.from_model(model)
+        tconfig = TrainConfig(n_epoch=300)
     logger = create_default_logger(pp, "train_voxel_autoencoder")
-    from pyinstrument import Profiler
-
-    profiler = Profiler()
-    profiler.start()
     train(pp, tcache, dataset, tconfig, num_workers=10)
-    profiler.stop()
-    print(profiler.output_text(unicode=True, color=True, show_all=False))
